f4d05721708ffff580376c9303adf27cd6729965
Osc-53219 <102975104+Osc-53219@users.noreply.github.com>
2023-09-18 22:04:50 -0500

    Fixed indentation errors and added quit functionality

--- main.py
+++ main.py
@@ -23,12 +23,20 @@ window = pygame.display.set_mode((WIDTH, HEIGHT))
 
 # Making the main function: We will run this to start the game
 def main(window):
-        clock = pygame.time.Clock()
+    clock = pygame.time.Clock()
 
         # This while loop will act as our event loop
-        run = True
-        while run:
-            clock.tick(FPS) # This ensures that our while loop is going to run no more than 60 FPS to regulate the frame rate across different devices
+    run = True
+    while run:
+        clock.tick(FPS) # This ensures that our while loop is going to run no more than 60 FPS to regulate the frame rate across different devices
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: # First event we will check for is if the user quit the game
+                run = False
+                break
+             
+    pygame.quit()
+    quit()
 
 # Purpose of this line in the function is to only call the main function if we run this file directly 
 if __name__ == "__main__":
